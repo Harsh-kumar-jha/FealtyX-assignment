a91192041b881d4a8cf3850cadb1a5fa76192ca6
Harsh Kumar Jha <jhaharshkumar293@gmail.com>
2025-07-06 12:25:55 +0530

    removed unused constants

--- src/main/java/com/FealtyX_assignment/Student/utils/Constants.java
+++ src/main/java/com/FealtyX_assignment/Student/utils/Constants.java
@@ -17,19 +17,11 @@ public class Constants {
     public static final LocalDateTime DEFAULT_DATE_TIME = LocalDateTime.now();
     public static final String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
     public static final String STUDENT_NOT_FOUND = "Student not found with id: ";
-    public static final String STUDENT_ALREADY_EXISTS = "Student already exists with id: ";
     public static final Optional OPTIONAL_EMPTY =  Optional.empty();
-    public static final Collector<Student, ?, List<Student>> STUDENT_COLLECTOR = Collectors.toList();
     public static final String EMAIL_PATTERN = "^[A-Za-z0-9+_.-]+@(.+)$";
     public static final String DELIMITER = ", ";
     public static final String STUDENT_NOT_NULL = "Student cannot be null";
     public static final String STUDENT_ID_NOT_NULL = "Student ID cannot be null";
-    public static final String STUDENT_ID_NOT_NULL_OR_EMPTY = "Student name cannot be null or empty";
-    public static final String STUDENT_AGE_NOT_NULL = "Student age cannot be null";
-    public static final String STUDENT_AGE_NOT_NEGATIVE = "Student age cannot be negative";
-    public static final String STUDENT_EMAIL_NOT_NULL_OR_EMPTY = "Student email cannot be null or empty";
-    public static final String EMAIL_INVALID = "Invalid email format";
-    public static final String NAME_NOT_EMPTY = "Name cannot be empty";
     public static final String EMAIL_ALREADY_EXISTS = "Email already exists: ";
     public static final String SEPARATOR = ": ";
 
